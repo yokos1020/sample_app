c37013050dac8e515188cfe6374a320a5e879925
yokos1020 <sbtyk1020@yahoo.co.jp>
2019-08-19 17:44:59 +0900

    20190819_3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,15 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+  
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
+      redirect_to @user
     else
       render :new
     end
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about' 
   get '/contact', to: 'static_pages#contact'
-  get '/signup', to: 'users#new' 
   get '/links', to: 'static_pages#links'
-
+  get '/signup', to: 'users#new' 
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
