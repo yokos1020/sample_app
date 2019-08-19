8ffd3ffe034679eb4917115927c9b41f154e35da
yokos1020 <sbtyk1020@yahoo.co.jp>
2019-08-19 11:59:01 +0900

    20190819

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'static_pages/links'
+
   get 'users/new'
 
   root 'static_pages#home' 
@@ -6,7 +8,8 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about' 
   get '/contact', to: 'static_pages#contact'
-  get '/signup', to: 'users#new'
+  get '/signup', to: 'users#new' 
+  get '/links', to: 'static_pages#links'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
