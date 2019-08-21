782ac0b86a64402a820e7f62b1ad8c24bb9e7a5f
yokos1020 <sbtyk1020@yahoo.co.jp>
2019-08-21 20:12:04 +0900

    20190821_4

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
+  
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "ログインしてください"
+      redirect_to root_url
+    end
+  end
+
+
 end
---
+++ app/controllers/microposts_controller.rb
@@ -0,0 +1,28 @@
+class MicropostsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
+
+  def create
+    @micropost = current_user.microposts.build(microposts_params)
+    if @micropost.save
+      flash[:success] = "投稿完了!!"
+      redirect_to root_url
+    else
+      render 'static_pages/home'
+    end
+  end
+
+  def destroy
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_url if @micropost.nil?
+    @micropost.destroy
+    flash[:success] = "投稿を削除しました"
+    redirect_to root_url
+  end
+
+
+  private
+
+  def microposts_params
+    params.require(:micropost).permit(:content)
+  end
+end
